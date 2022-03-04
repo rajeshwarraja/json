b31bc0d3145418c545e17262afc27745d9d1b3c2
Rajeshwar Raja <rajeshwarraja@LIVE.COM>
2022-03-04 22:21:06 +0530

    Support for GitHub actions for GIT details generation

--- build/modules/GitInformation.cmake
+++ build/modules/GitInformation.cmake
@@ -1,11 +1,15 @@
 function(_get_git_branch _branch)
-	execute_process(
-		COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-		OUTPUT_VARIABLE BRANCH
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	set(${_branch} ${BRANCH} PARENT_SCOPE)
+	if(NOT "$ENV{GITHUB_HEAD_REF}" STREQUAL "")
+		set(${_branch} $ENV{GITHUB_HEAD_REF} PARENT_SCOPE)
+	else()
+		execute_process(
+			COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+			OUTPUT_VARIABLE BRANCH
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		set(${_branch} ${BRANCH} PARENT_SCOPE)
+	endif()
 endfunction()
 
 function(_normalize_branch_name _out_name _in_name)
@@ -19,14 +23,18 @@ function(get_git_version _version)
 		set(PROJECT_VERSION "0.0.0")
 	endif()
 
-	execute_process(
-		COMMAND "${GIT_EXECUTABLE}" rev-list --count HEAD
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-		OUTPUT_VARIABLE NCOMMIT
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	if("${NCOMMIT}" STREQUAL "")
-		set(NCOMMIT "0")
+	if(NOT "$ENV{GITHUB_RUN_NUMBER}" STREQUAL "")
+		set(NCOMMIT "$ENV{GITHUB_RUN_NUMBER}")
+	else()
+		execute_process(
+			COMMAND "${GIT_EXECUTABLE}" rev-list --count HEAD
+			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+			OUTPUT_VARIABLE NCOMMIT
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if("${NCOMMIT}" STREQUAL "")
+			set(NCOMMIT "0")
+		endif()
 	endif()
 
 	# calculate suffix for GitFlow
@@ -46,24 +54,32 @@ function(get_git_version _version)
 endfunction()
 
 function(get_git_repository_url _url)
-	execute_process(
-		COMMAND "${GIT_EXECUTABLE}" config --get remote.origin.url
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-		OUTPUT_VARIABLE URL
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	set(${_url} ${URL} PARENT_SCOPE)
+	if(NOT "$ENV{GITHUB_SERVER_URL}" STREQUAL "")
+		set(${_url} "$ENV{GITHUB_SERVER_URL}/$ENV{GITHUB_REPOSITORY}.git" PARENT_SCOPE)
+	else()
+		execute_process(
+			COMMAND "${GIT_EXECUTABLE}" config --get remote.origin.url
+			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+			OUTPUT_VARIABLE URL
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		set(${_url} ${URL} PARENT_SCOPE)
+	endif()
 endfunction()
 
 function(get_git_commit _hash)
+	if(NOT "$ENV{GITHUB_SHA}" STREQUAL "")
+		set(${_hash} $ENV{GITHUB_SHA} PARENT_SCOPE)
+	else()
 	_get_git_branch(BRANCH)
-	execute_process(
-		COMMAND "${GIT_EXECUTABLE}" rev-parse ${BRANCH}
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-		OUTPUT_VARIABLE HASH
-		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	set(${_hash} ${HASH} PARENT_SCOPE)
+		execute_process(
+			COMMAND "${GIT_EXECUTABLE}" rev-parse ${BRANCH}
+			WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+			OUTPUT_VARIABLE HASH
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		set(${_hash} ${HASH} PARENT_SCOPE)
+	endif()
 endfunction()
 
 function(display_project_label)
