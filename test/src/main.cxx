3ca98f2ce877413f2580f553111c30ff61456aa9
Rajeshwar Raja <rajeshwarraja@LIVE.COM>
2022-03-05 14:13:45 +0530

    Refactored version information generation

--- test/src/main.cxx
+++ test/src/main.cxx
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include <version.h>
 
 int main(int argc, char* argv[])
 {
+    std::cout << "Test: " << gitinfo << std::endl;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
